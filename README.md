1ce94f588f072d0e03a4c4e1aca252f9bed7c6bb
jackbeau <63471281+jackbeau@users.noreply.github.com>
2023-02-01 15:37:01 +0000

    initial

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# Python Implementation of Emotion Detection with EVM
+
+## Dependencies
+
++ scipy
++ scikit-image
++ opencv
++ numpy
++ pyrtools
++ tensorflow
+
+## Execution
+Just run the __innit__.py file
+
+## Acknowledgements 
+- https://github.com/absdnd/Eulerian_Motion_Magnification
+- https://github.com/atulapra/Emotion-detection
---
+++ __innit__.py
@@ -0,0 +1,39 @@
+import cv2                
+import time
+import sys, os
+
+sys.path.append(os.path.join(sys.path[0],'emotion_detection'))
+sys.path.append(os.path.join(sys.path[0],'evm'))
+from emotion_detection.emotions import emotion_detection
+from evm.magnification import Magnify
+
+fps = 8.
+alpha = 300
+lambda_c = 200
+fl = 0.3
+fh = 1.
+cam = cv2.VideoCapture(0)
+_,img = cam.read()
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+s = Magnify(gray,alpha,lambda_c,fl,fh,fps)
+
+while True:
+    t1 = time.process_time()
+    _,img = cam.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    evm = s.Magnify(gray)
+    raw_emo = emotion_detection(gray)
+    evm_emo = emotion_detection(evm)
+
+    cv2.imshow('Raw', raw_emo)
+    cv2.imshow('EVM', evm_emo)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    t2 = time.process_time()
+    
+    print("set fps",1./(t2-t1))
+
+cam.release()
+cv2.destroyAllWindows()
