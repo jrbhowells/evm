86d913908adee73d3565236731557e3aa43a1908
jrbhowells <73129910+jrbhowells@users.noreply.github.com>
2023-02-01 21:50:25 +0000

    Working with colour

--- __init_col__.py
+++ __init_col__.py
@@ -1,6 +1,7 @@
 import cv2                
 import time
 import sys, os
+import numpy as np
 
 sys.path.append(os.path.join(sys.path[0],'emotion_detection'))
 sys.path.append(os.path.join(sys.path[0],'evm'))
@@ -17,13 +18,21 @@ _,img = cam.read()
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 s = Magnify(gray,alpha,lambda_c,fl,fh,fps)
 
+
+
 while True:
     t1 = time.process_time()
     _,img = cam.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    evm = s.Magnify(gray)
-    raw_emo = emotion_detection(gray)
-    evm_emo = emotion_detection(evm)
+
+    evmb = s.Magnify(img[:,:,0]) # Blue component, magnified
+    evmg = s.Magnify(img[:,:,1]) # Green component, magnified
+    evmr = s.Magnify(img[:,:,2]) # Red component, magnified
+
+    # Combine magnified elements to full colour
+    bgr = np.dstack((evmb, evmg, evmr))
+
+    raw_emo = emotion_detection(img)
+    evm_emo = emotion_detection(bgr)
 
     cv2.imshow('Raw', raw_emo)
     cv2.imshow('EVM', evm_emo)
--- emotion_detection/emotions.py
+++ emotion_detection/emotions.py
@@ -46,12 +46,10 @@ def emotion_detection(img):
 
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
-        roi_img = img[y:y + h, x:x + w]
+        roi_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)[y:y + h, x:x + w]
         cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_img, (48, 48)), -1), 0)
         prediction = model.predict(cropped_img)
         maxindex = int(np.argmax(prediction))
         cv2.putText(img, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-    # faces = facecasc.detectMultiScale(img,scaleFactor=1.3, minNeighbors=5)
 
     return img
